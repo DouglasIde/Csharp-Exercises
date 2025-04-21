127bb51088fbd078c9afe0de51d7cb0b09c30972
DouglasIde <douglasymide@gmail.com>
2025-04-21 12:51:21 -0300

    Feat: Completing the first challenge

---
+++ OOP/Creating my first application/1) HelloWorld/helloworld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Helloworld
+{
+    public static void Main()
+    {
+        string curso = "Ciência da Computação";
+        string nome = "Douglas";
+        string sobrenome = "Yugo";
+
+        Console.WriteLine($"O aluno {nome} {sobrenome}, está realizando o curso: {curso}");
+    }
+}
